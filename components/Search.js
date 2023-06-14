c6de8b1af7d280e273737cdc3355beff095c1929
romane 29git <rcotecoliss@ensc.fr>
2023-06-14 18:07:49 +0200

    recherche avec filtre

--- components/Search.js
+++ components/Search.js
@@ -42,13 +42,98 @@ const Search = () => {
         onChangeText={(value) => handleSearch(value)}
       />
       {searchResults.length > 0 && searchTerm.length >= 3 ? (
-        <FlatList
-          data={searchResults}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => (
-            <Text style={styles.item}>{item.name}</Text>
-          )}
-        />
+        <View>
+          <FlatList
+            data={searchResults}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => {
+              if (item.type === "company") {
+                return <Text style={styles.item}>{item.name}</Text>;
+              }
+              return null;
+            }}
+            ListHeaderComponent={<Text>Entreprises :</Text>}
+          />
+
+          <FlatList
+            data={searchResults}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => {
+              if (item.type === "list") {
+                return <Text style={styles.item}>{item.name}</Text>;
+              }
+              return null;
+            }}
+            ListHeaderComponent={<Text>Listes :</Text>}
+          />
+
+          <FlatList
+            data={searchResults}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => {
+              if (item.type === "email_list") {
+                return <Text style={styles.item}>{item.name}</Text>;
+              }
+              return null;
+            }}
+            ListHeaderComponent={<Text>Liste d'email :</Text>}
+          />
+
+          <FlatList
+            data={searchResults}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => {
+              if (item.type === "email_contact") {
+                return <Text style={styles.item}>{item.name}</Text>;
+              }
+              return null;
+            }}
+            ListHeaderComponent={<Text>Mail :</Text>}
+          />
+
+          <FlatList
+            data={searchResults}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => {
+              if (item.type === "contact") {
+                return <Text style={styles.item}>{item.name}</Text>;
+              }
+              return null;
+            }}
+            ListHeaderComponent={<Text>Contact :</Text>}
+          />
+
+          <FlatList
+            data={searchResults}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => {
+              if (item.type === "campaign") {
+                return <Text style={styles.item}>{item.name}</Text>;
+              }
+              return null;
+            }}
+            ListHeaderComponent={<Text>Campagnes :</Text>}
+          />
+
+          <FlatList
+            data={searchResults}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => {
+              if (
+                item.type != "company" &&
+                item.type != "list" &&
+                item.type != "email_list" &&
+                item.type != "email_contact" &&
+                item.type != "contact" &&
+                item.type != "campaign"
+              ) {
+                return <Text style={styles.item}>{item.name}</Text>;
+              }
+              return null;
+            }}
+            ListHeaderComponent={<Text>Autres :</Text>}
+          />
+        </View>
       ) : (
         <Text>Aucun résultat trouvé.</Text>
       )}
